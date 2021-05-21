77441904fca78ba9001b937d6bcb614163adec37
s736828@gmail.com <s736828@gmail.com>
2021-05-21 15:45:39 +0800

    A11087

---
+++ Airiti_msit_129/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Airiti_msit_129 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("hi, 我是爲中");
+
+            Console.WriteLine("Leo Shen");
+
+            Console.WriteLine("hi, 我是亮均");
+        }
+    }
+}
