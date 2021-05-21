8cb3a50a72ccdc6e8c2fa4c94c885eadb2e07905
alwaysCwC <always0537@gmail.com>
2021-05-21 15:11:18 +0800

    WC, A11083, 簽到

--- Airiti_msit_129/Program.cs
+++ Airiti_msit_129/Program.cs
@@ -4,7 +4,11 @@ namespace Airiti_msit_129 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+
             Console.WriteLine("hi, 我是爲中");
+
+            Console.WriteLine("Leo Shen");
+
         }
     }
 }
