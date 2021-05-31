9bc891dcf1fbca0ea4cea270f5fc921b432bdd2f
Cloud <alice123557@gmail.com>
2021-05-31 13:30:19 +0800

    111

---
+++ SchemaNote_11086/ViewModels/inputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchemaNote_11086.ViewModels
+{
+    public class inputModel
+    {
+       public List<SchemaNoteViewModel>  show { get; set; }
+
+
+    }
+}
