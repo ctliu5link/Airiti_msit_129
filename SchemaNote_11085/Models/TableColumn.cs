4b3b4704189221afbe2844e6f5dd0af51be0da0d
flame32916 <flame329166@gmail.com>
2021-06-06 15:54:48 +0800

    Commit

---
+++ SchemaNote_11085/Models/TableColumn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchemaNote_11085.Models
+{
+    public class TableColumn
+    {
+        public string TableName { get; set; }
+        public string Main_UserTable { get; set; }
+        public string DescriptionName { get; set; }
+        public string Object_CreateDay { get; set; }
+        public string Object_UpdateDay { get; set; }
+        public string TotalCount { get; set; }
+        public string Remark { get; set; }
+    }
+}
