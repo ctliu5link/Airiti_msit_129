4d1bdd1becf9389c7a897711a5c7087abad7418d
jerry <s736828@gmail.com>
2021-06-10 16:33:53 +0800

    0610 update

--- SchemaNote_11087/Models/TablePropertyEditModel.cs
+++ SchemaNote_11087/Models/TablePropertyEditModel.cs
@@ -8,7 +8,7 @@
         public string Item { get; set; } //欄位擴充屬性'名稱'
         public string Value { get; set; } //欄位擴充屬性'值'
         public string TableMSDescription { get; set; }//資料表擴充屬性'名稱'
-        public string TableRemark { get; set; }//資料表擴充屬性'值'...........
+        public string TableRemark { get; set; }//資料表擴充屬性'值'
 
     }
 }
