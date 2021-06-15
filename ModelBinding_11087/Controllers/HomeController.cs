05bc33cd7bfe808775cc64f21b58a591b437079e
flame32916 <flame329166@gmail.com>
2021-06-15 18:26:35 +0800

    Test

--- ModelBinding_11087/Controllers/HomeController.cs
+++ ModelBinding_11087/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace ModelBinding_11087.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
