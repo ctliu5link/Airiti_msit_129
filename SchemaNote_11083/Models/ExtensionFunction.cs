1d7d1500910c0843b449731bb7521f32cec081ab
alwaysCwC <always0537@gmail.com>
2021-06-29 19:45:17 +0800

    update

--- SchemaNote_11083/Models/ExtensionFunction.cs
+++ SchemaNote_11083/Models/ExtensionFunction.cs
@@ -9,34 +9,34 @@ namespace SchemaNote_11083.Models
 
     public static class ExtensionFunction
     {
-        public static List<T> DataMapping<T>(this DataTable table)
+        public static List<T> DataMapping<T>(this DataTable p_table)
         {
 
-            List<T> tables = new List<T>();
+            List<T> Table = new List<T>();
 
-            foreach (DataRow row in table.Rows)
+            foreach (DataRow row in p_table.Rows)
             {
-                var newTable = Activator.CreateInstance<T>();
+                var newRow = Activator.CreateInstance<T>();
                 foreach (var property in typeof(T).GetProperties())
                 {
                     switch (property.PropertyType.Name)
                     {
                         case "String":
-                            property.SetValue(newTable, row[property.Name].ToString());
+                            property.SetValue(newRow, row[property.Name].ToString());
                             break;
                         case "Int32":
-                            property.SetValue(newTable, (int)row[property.Name]);
+                            property.SetValue(newRow, (int)row[property.Name]);
                             break;
                         case "Int64":
-                            property.SetValue(newTable, (long)row[property.Name]);
+                            property.SetValue(newRow, (long)row[property.Name]);
                             break;
                         default:
                             break;
                     }
                 }
-                tables.Add(newTable);
+                Table.Add(newRow);
             }
-            return tables;
+            return Table;
         }
 
     }
