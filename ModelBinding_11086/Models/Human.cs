42ebd4cf83c1159c6d2420aee8496ed71e55ae41
Cloud <alice123557@gmail.com>
2021-07-22 11:16:28 +0800

    updata

---
+++ ModelBinding_11086/Models/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModelBinding_11086.Models
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    public class Person : Human
+    {
+        public List<Person> Friends { get; set; }
+    }
+
+}
