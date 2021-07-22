3c6e7491c86fc39abbd22043dc686a924a3d90b7
rovingwind-PC <rovingwindownload@gmail.com>
2021-07-22 08:35:43 +0800

    update

---
+++ ModelBinding_11084/Controllers/DefaultController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelBinding_11084.Models;
+using ModelBinding_11084.ViewModels;
+
+namespace ModelBinding_11084.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            TemplateViewModel SimpleBinding = new TemplateViewModel("SimpleBinding", "(string Name, int Age)", new { Name = "Tom", Age = 15 }, "text");
+            Human Tom = new Human() { Name = "Tom", Age = 15 };
+            TemplateViewModel ModelBindObj = new TemplateViewModel("ModelBindObj", "(Human data)", Tom, "json");
+            TemplateViewModel SimpleModelBindArray = new TemplateViewModel("SimpleModelBindArray", "(string[] Name, int[] Age)", new { Name = new string[] { "Tom", "Jack" }, Age = new int[] { 15, 16 } }, "text");
+
+            return View(TemplateViewModel.lisTemplate);
+        }
+        public ActionResult SimpleBinding(string Name, int Age)
+        {
+            return Content(nameof(Name) + ":" + Name + ", " + nameof(Age) + ":" + Age);
+        }
+
+        public ActionResult ModelBindObj(Human data)
+        {
+            return Json(data);
+        }
+
+        public ActionResult SimpleModelBindArray(string[] Name, int[] Age)
+        {
+            string str = nameof(Name) + ":";
+            foreach (string n in Name)
+            {
+                str += n + ",";
+            }
+            str = str.Substring(0, str.Length - 1);
+            str += " | ";
+            str += nameof(Age) + ":";
+            foreach (int a in Age)
+            {
+                str += a + ",";
+            }
+            str = str.Substring(0, str.Length - 1);
+            return Content(str);
+        }
+
+        public ActionResult ModelBindingArray(Human[] data)
+        {
+            return Json(data);
+        }
+
+        public ActionResult ModelBindingNestedObj(Person data)
+        {
+            return Json(data);
+        }
+
+        public ActionResult ModelBindingArrayNestedObj(Person[] data)
+        {
+            return Json(data);
+        }
+
+
+
+        public ActionResult SendFile(HttpPostedFileBase fileBase)
+        {
+            byte[] bytes = new byte[0];
+            string fileName = "";
+            if (fileBase != null)
+            {
+                if (fileBase.ContentLength > 0)
+                {
+                    bytes = new byte[fileBase.ContentLength];
+                    fileBase.InputStream.Read(bytes, 0, fileBase.ContentLength);
+                    //fileBase.SaveAs(System.Web.HttpContext.Current.Server.MapPath("~/Files/" + fileBase.FileName));
+                    fileBase.InputStream.Flush();
+                }
+                fileName = fileBase.FileName;
+            }
+            return File(bytes, System.Net.Mime.MediaTypeNames.Application.Octet/*application/octet-stream*/, fileName);
+        }
+
+        public ActionResult SendByte()
+        {
+            var r = Request;
+            byte[] bytes = r.BinaryRead(r.ContentLength);
+            return File(bytes, System.Net.Mime.MediaTypeNames.Application.Octet/*application/octet-stream*/, "img.png");
+        }
+
+    }
+}
---
+++ ModelBinding_11084/ViewModels/TemplateViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ModelBinding_11084.ViewModels
+{
+    public class TemplateViewModel
+    {
+        /// <summary>
+        /// 繫結類型
+        /// </summary>
+        public string bindingType { get; set; }
+        /// <summary>
+        /// JSON value
+        /// </summary>
+        public HtmlString objJSON { get; set; }
+        /// <summary>
+        /// Action參數
+        /// </summary>
+        public string actionParameter { get; set; }
+        /// <summary>
+        /// 返回類型
+        /// </summary>
+        public string returnType { get; set; }
+        /// <summary>
+        /// 傳給前端的 List<TemplateViewModel>
+        /// </summary>
+        public static List<TemplateViewModel> lisTemplate = new List<TemplateViewModel>();
+        /// <summary>
+        /// 建構式
+        /// </summary>
+        /// <param name="_bindingType"></param>
+        /// <param name="_actionParameter"></param>
+        /// <param name="_object"></param>
+        /// <param name="_returnType"></param>
+        public TemplateViewModel(string _bindingType, string _actionParameter, object _object, string _returnType)
+        {
+            bindingType = _bindingType;
+            actionParameter = _actionParameter;
+            objJSON = new HtmlString(JsonConvert.SerializeObject(_object));            
+            returnType = _returnType;
+
+            lisTemplate.Add(this);
+        }
+        
+    }
+}
